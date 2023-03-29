b263d16f83e6afd78d095d7db04d9353d25e6a39
Matheus Fidelis <matheus@nanoshots.com.br>
2023-03-28 23:36:33 -0300

    Istio and Some Charts

--- variables.tf
+++ variables.tf
@@ -3,20 +3,24 @@ variable "cluster_name" {
 }
 
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS Region"
+  default     = "us-east-1"
 }
 
 variable "k8s_version" {
-  default = "1.20"
+  description = "Default Kubernetes Version"
+  default     = "1.24"
 }
 
 variable "nodes_instances_sizes" {
+  description = "List of family instance sizes. Fixed in **.g.instance"
   default = [
     "c6g.large"
   ]
 }
 
 variable "auto_scale_options" {
+  description = "Auto Scaling Options"
   default = {
     min     = 4
     max     = 10
@@ -25,34 +29,64 @@ variable "auto_scale_options" {
 }
 
 variable "nlb_ingress_internal" {
-  type    = bool
-  default = false
+  description = "Flag to create internal or external NLB"
+  type        = bool
+  default     = false
 }
 
 variable "nlb_ingress_type" {
-  type    = string
-  default = "network"
+  description = "Network Load Balancer"
+  type        = string
+  default     = "network"
 }
 
 variable "proxy_protocol_v2" {
-  type    = bool 
-  default = false
+  description = "Proxy Protocol v2 Enable"
+  type        = bool
+  default     = false
 }
 
 variable "volume_size" {
-  type    = number
-  default = 50
+  description = "EBS size for EC2 nodes instances"
+  type        = number
+  default     = 50
 }
 
 variable "volume_type" {
-  type    = string
-  default = "gp3"
+  description = "EBS volume type"
+  type        = string
+  default     = "gp3"
 }
 
 // Graviton2
 variable "ami_type" {
-  type    = string
-  default = "AL2_ARM_64"
+  description = "AMI Type. Need's to be ARM_64"
+  type        = string
+  default     = "AL2_ARM_64"
+}
+
+variable "addon_cni_version" {
+  type        = string
+  description = "VPC CNI Version"
+  default     = "v1.12.6-eksbuild.1"
+}
+
+variable "addon_coredns_version" {
+  type        = string
+  description = "CoreDNS Version"
+  default     = "v1.8.7-eksbuild.4"
+}
+
+variable "addon_kubeproxy_version" {
+  type        = string
+  description = "Kubeproxy Version"
+  default     = "v1.22.17-eksbuild.2"
+}
+
+variable "addon_csi_version" {
+  type        = string
+  description = "CSI Version"
+  default     = "v1.17.0-eksbuild.1"
 }
 
 variable "nlb_ingress_enable_termination_protection" {
@@ -65,14 +99,28 @@ variable "enable_cross_zone_load_balancing" {
   default = true
 }
 
+// Grafana
+
+variable "grafana_virtual_service_host" {
+  type    = string
+  default = "grafana.k8s.raj.ninja"
+}
+
+// Kiali
 variable "kiali_virtual_service_host" {
-  type    = string 
+  type    = string
   default = "kiali.k8s.raj.ninja"
 }
 
 variable "grafana_kiali_virtual_service_host" {
-  type    = string 
+  type    = string
   default = "grafana.kiali.k8s.raj.ninja"
+}
+
+// Jaeger
+variable "jaeger_virtual_service_host" {
+  type    = string
+  default = "jaeger.k8s.raj.ninja"
 }
 
 variable "default_tags" {
